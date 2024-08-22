020e4b92a196f962166d528af9e6069fb83b7692
Tristan <tristankrauss123@gmail.com>
2024-08-22 10:43:10 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+morse_code_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..',
+    'J': '.---', 'K': '-.-', 'L': '.-..',  'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+    '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----',
+
+    ' ': '|'
+}
+
+to_be_converted = input("Enter a message here: \n").upper()
+morse_code = ''
+print(to_be_converted)
+
+for n in to_be_converted:
+    if n in morse_code_dict:
+        morse_code = morse_code + morse_code_dict[n] + ' '
+
+print(morse_code)
